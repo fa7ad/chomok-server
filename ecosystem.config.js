580ca4b27d465b693cbc90bb1db6030a8243ce29
Fahad Hossain <evilinfinite7@gmail.com>
2018-09-16 17:56:24 +0600

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Chomok-server',
+      script: 'server.js',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3333,
+        DB_PREFIX: 'chomok_'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3003,
+        DB_PREFIX: 'chomok_',
+        CWD: '/home/node-www/chomox-server'
+      }
+    }
+  ]
+}
